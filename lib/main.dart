d6ef76125751c036a327648ed1759f5a1d1bc930
blackbox <dzzirima@gmail.com>
2022-06-29 17:43:15 +0200

    feat :customised onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:at_commons/at_commons.dart';
 import 'package:at_onboarding_flutter/at_onboarding_flutter.dart'
     show Onboarding;
 import 'package:at_utils/at_logger.dart' show AtSignLogger;
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:path_provider/path_provider.dart'
     show getApplicationSupportDirectory;
 import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
@@ -66,35 +67,72 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       // * The onboarding screen (first screen)
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Securely Track Your Activities'),
-          centerTitle: true,
-        ),
-        body: Builder(
-          builder: (context) => Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                var preference = await futurePreference;
-                setState(() {
-                  atClientPreference = preference;
-                });
-                Onboarding(
-                  context: context,
-                  atClientPreference: atClientPreference!,
-                  domain: AtEnv.rootDomain,
-                  rootEnvironment: AtEnv.rootEnvironment,
-                  appAPIKey: AtEnv.appApiKey,
-                  onboard: (value, atsign) {
-                    _logger.finer('Successfully onboarded $atsign');
+        // appBar: AppBar(
+        //   title: const Text('Securely Track Your Activities'),
+        //   centerTitle: true,
+        // ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              const Spacer(),
+              Image.asset(
+                "assets/meditatation.png",
+                height: 250,
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "The key is in not spending time,\nbut in investing it..",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                "The shorter way to do many things is to only do \n one thing at a time.",
+                textAlign: TextAlign.center,
+              ),
+              const Spacer(),
+              SizedBox(
+                height: 60,
+                width: 100,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(shape: CircleBorder()),
+                  onPressed: () async {
+                    var preference = await futurePreference;
+                    setState(() {
+                      atClientPreference = preference;
+                    });
+                    Onboarding(
+                      context: context,
+                      atClientPreference: atClientPreference!,
+                      domain: AtEnv.rootDomain,
+                      rootEnvironment: AtEnv.rootEnvironment,
+                      appAPIKey: AtEnv.appApiKey,
+                      onboard: (value, atsign) {
+                        _logger.finer('Successfully onboarded $atsign');
+                      },
+                      onError: (error) {
+                        _logger.severe('Onboarding throws $error error');
+                      },
+                      nextScreen: const BottomBar(),
+                    );
                   },
-                  onError: (error) {
-                    _logger.severe('Onboarding throws $error error');
-                  },
-                  nextScreen: const BottomBar(),
-                );
-              },
-              child: const Text('Onboard an @sign'),
-            ),
+                  // child: SvgPicture.asset(
+                  //   "/closes.svg",
+                  //   fit: BoxFit.contain,
+                  // ),
+                  child: Text("Hello"),
+                ),
+              ),
+              const Spacer(),
+            ],
           ),
         ),
       ),
